f217c574e35c4acc4e81a79df40f9f6c599269b7
Alexander <nosorog-2010@yandex.ru>
2022-08-29 10:59:21 +0300

    Save number%10

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 || number > 999)
+{
+    Console.WriteLine("Это не трёхзначное число, введите новое значение");
+}
+else
+{
+int a = number%10;
+Console.WriteLine("последняя цифра данного числа " +a);
+}
