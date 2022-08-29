4ec896d539a97fef215af04e8489e332c630e066
Alexander <nosorog-2010@yandex.ru>
2022-08-29 09:45:11 +0300

    Save number N

---
+++ Examples005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на входе принимает одно число N, а на выходе отображает все целые числа
+// в промежутке от -N до N 
+// numberNeg++ это тоже самое что numberNeg=numberNeg+1, то есть добавляется ещё один цикл
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int numberNeg = number*(-1);
+
+while(numberNeg <= number)
+{
+    Console.WriteLine(numberNeg);
+    numberNeg++;
+}
