11f1b136e16635375cfea62e0c7d3b19e7841327
Alexander <nosorog-2010@yandex.ru>
2022-08-29 06:06:17 +0300

    Save ToInt32 sqr

---
+++ Exsample001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Конвертируем Convert.ToInt32 переводим строковый тип данных в числовой
+// Когда указываем в Console.WriteLine() текс русским алфавитом, то обязательно ставим его в кавычки ""
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqr = number*number;
+
+Console.WriteLine("Квадрат данного числа");
+
+Console.WriteLine(sqr);
