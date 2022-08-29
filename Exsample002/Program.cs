1eb95b04e6227c0944f0ef72be084d26ec810f06
Alexander <nosorog-2010@yandex.ru>
2022-08-29 06:43:23 +0300

    Save max a or b

---
+++ Exsample002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.WriteLine(" Первое число " + a + " больше, чем второе " + b );
+}
+
+else
+
+{
+    Console.WriteLine(" Второе число " + b + " больше, чем первое " + a);
+}
